b73502b65f9a7198fc9d84113354cb9fddcadc7a
RutgersDee <12137270+rutgersDee@users.noreply.github.com>
2018-08-22 17:17:17 -0400

    fixed settings patterns and sync

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -251,7 +251,7 @@ $databases = array();
  *   );
  * @endcode
  */
-$config_directories = array();
+$config_directories[CONFIG_SYNC_DIRECTORY] = '../config/sync';
 
 /**
  * Settings:
@@ -745,6 +745,7 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  * will allow the site to run off of all variants of example.com and
  * example.org, with all subdomains included.
  */
+$settings['trusted_host_patterns'] = ['^.*\.dd$'];
 
 /**
  * The default list of directories that will be ignored by Drupal's file API.
